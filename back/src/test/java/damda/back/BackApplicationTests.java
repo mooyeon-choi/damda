db56118282feec216ac5646217af068ac39d3179
mooyeon-choi <dus1208@ajou.ac.kr>
2021-10-26 01:30:30 +0900

    Init Backend

---
+++ back/src/test/java/damda/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package damda.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
