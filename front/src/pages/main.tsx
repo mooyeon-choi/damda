b84b4a6ab872976c0b938ca9ced13410f60f6ba4
mooyeon-choi <lon12080@gmail.com>
2021-11-09 21:32:05 +0900

    Styled Components 추가

--- front/src/pages/main.tsx
+++ front/src/pages/main.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
 import logo from './../damdaticon.png';
+import styled from 'styled-components';
 
 interface IMainProps {
 }
 
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+// Create a Wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
 const Main: React.FunctionComponent<IMainProps> = (props) => {
-  return (<div>
+  return (<Wrapper>
     <img src={logo} className="App-logo" alt="logo" />
     <p>
       Hi~
@@ -18,7 +31,7 @@ const Main: React.FunctionComponent<IMainProps> = (props) => {
     >
       Learn React
     </a>
-  </div>);
+  </Wrapper>);
 };
 
 export default Main;
